6f38c3df169e415f70c7b3ff1895fa51b4e342d2
Yuki Hagihara <m213879@hiroshima-u.ac.jp>
2021-06-30 03:34:41 +0000

    Complete input function

--- tictactoe.go
+++ tictactoe.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	//"strings"
+	"strconv"
+	"strings"
 )
 
 type Board struct {
@@ -12,22 +13,22 @@ type Board struct {
 //func (b *Board) put(x, y int, u string) // interface
 //func (b *Board) get(x, y int) string    // interface
 
-func input(n_m string) int {
-	//	s := strings.Split(n_m, ",")
-	x := 1
-	// y := 2
+func input(n_m string) [2]int {
+	var x [2]int
+	s := strings.Split(n_m, ",")
+	x[0], _ = strconv.Atoi(s[0])
+	x[1], _ = strconv.Atoi(s[1])
 	return x
 }
 
-/*
 func (b *Board) put(x, y int, u string) {
 
-	if x >= 0 && x <= 2 && y >= 0 && y <= 2 {
-		//エラー
-	}
-	if b.tokens[x+3*y] != 0 {
-		//エラー
-	}
+	//	if x >= 0 && x <= 2 && y >= 0 && y <= 2 {
+	//エラー
+	//	}
+	//	if b.tokens[x+3*y] != 0 {
+	//エラー
+	//	}
 
 	if u == "o" {
 		b.tokens[x+3*y] = 1
@@ -35,9 +36,24 @@ func (b *Board) put(x, y int, u string) {
 		b.tokens[x+3*y] = -1
 	}
 
-	return b
+	//return b*
 }
-*/
+
+func printBoard(b *Board) {
+	for i := 0; i < 9; i++ {
+		if b.tokens[i] == 0 {
+			fmt.Print(".")
+		} else if b.tokens[i] == 1 {
+			fmt.Print("o")
+		} else if b.tokens[i] == -1 {
+			fmt.Print("x")
+		}
+		if (i+1)%3 == 0 {
+			fmt.Println()
+		}
+	}
+}
+
 func main() {
 	//		var board Board
 	//		board = ['.','.','.']
@@ -45,4 +61,7 @@ func main() {
 	print("Player1 :input (x,y) ")
 	fmt.Scan(&n_m)
 	fmt.Println(input(n_m))
+
+	//x := input(n_m)
+	//	put(x[0], x[1], "o")
 }
--- tictactoe_test.go
+++ tictactoe_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestEx01(t *testing.T) {
-	expect := 1
-	result := input("1")
+	expect := [2]int{1, 1}
+	result := input("1,1")
 	if result != expect {
 		t.Error("Test01 is failed")
 	}
